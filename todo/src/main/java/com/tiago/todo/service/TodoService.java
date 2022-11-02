7eb3b45b0f01218f1774a4c84d50406fc86beb14
TIAGODEOVM22 <tiagodeoliveiravillalva@gmail.com>
2022-11-02 16:00:06 -0300

    implementação do endpoint update

--- todo/src/main/java/com/tiago/todo/service/TodoService.java
+++ todo/src/main/java/com/tiago/todo/service/TodoService.java
@@ -12,42 +12,48 @@ import com.tiago.todo.service.exception.ObjectNotFoundException;
 
 @Service
 public class TodoService {
-	
+
 	@Autowired
 	private TodoRepository repository;
 
-	public Todo findById (Long id) {
-		Optional <Todo> obj = repository.findById(id);
+	public Todo findById(Long id) {
+		Optional<Todo> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id:" + id +", tipo:"
-				+ Todo.class.getName()));
+				"Objeto não encontrado! Id:" + id + ", tipo:" + Todo.class.getName()));
 	}
-	
-	public List<Todo> findAllOpen(){
+
+	public List<Todo> findAllOpen() {
 		List<Todo> list = repository.findAllOpen();
 		return list;
 	}
-	
-	public List<Todo> finAllClose(){
+
+	public List<Todo> finAllClose() {
 		List<Todo> list = repository.findAllClose();
 		return list;
 	}
-	
-	public List<Todo> findAll(){
-		List <Todo> list = repository.findAll();
+
+	public List<Todo> findAll() {
+		List<Todo> list = repository.findAll();
 		return list;
 	}
 
 	public Todo create(Todo obj) {
-		obj.setId(null);/*medida de segurança para o usuario não informar o ID*/
+		obj.setId(null);/* medida de segurança para o usuario não informar o ID */
 		return repository.save(obj);
 	}
 
 	public void delete(Long id) {
-	repository.deleteById(id);
-		
-		
+		repository.deleteById(id);
+
 	}
-	
-	
+
+	public Todo update(Long id, Todo obj) {
+		Todo newObj = findById(id);/* faz a verificação se realmente existe o id informado */
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setDescricao(obj.getDescricao());
+		newObj.setDataParaFinalizar(obj.getDataParaFinalizar());
+		newObj.setFinalizado(obj.getFinalizado());
+		return repository.save(newObj);
+	}
+
 }
